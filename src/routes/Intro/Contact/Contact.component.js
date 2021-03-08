3cb42a2d057da1edfd17cf2c36627c14ed6793d0
Long Vu <vuhailong224@gmail.com>
2021-03-08 15:49:33 +0200

    send email spinner

--- src/routes/Intro/Contact/Contact.component.js
+++ src/routes/Intro/Contact/Contact.component.js
@@ -1,8 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import { ModalInfo } from '../../../components/Modal2/Modal'
 import { Input, TextArea } from "../../../components/Input";
-import { Link } from 'react-router-dom';
+import BarLoader from "react-spinners/BarLoader";
+import { css } from "@emotion/core";
 import './Contact.scss'
+
+const overrideStyle = css`
+    display: block;
+    margin: 0 auto;
+    border-color: red;
+    z-index: -1;
+`;
 
 const Contact = (props) => {
 
@@ -43,12 +51,6 @@ const Contact = (props) => {
             <div className="contact-root">
                 <div className="contact-wrap container">
                     <div className="contact-side">
-                        <div>
-                        <Link to='/' className='social-logo contact-side-logo'>
-                            Cabbage 
-                            <i className='fas fa-paw' />
-                        </Link>
-                        </div>
                         <div className="contact-side-form">
                         <h3>Get in touch with Cabbage</h3>
                         <Input 
@@ -65,6 +67,7 @@ const Contact = (props) => {
                             placeholder="Message" 
                             onChange={(e) => setMessage(e.target.value)} />
                         <button onClick={handleSubmit}>Submit</button>
+                        <BarLoader color={"#4A4A4A"} loading={props.contact.loading} width={200} css={overrideStyle} size={100} />
                         </div>
                     </div>
                     <div className="contact-title">
